c59781ce7602f106a70b5e0b2de94801dac87655
Collin Glass <collinglass@gmx.com>
2014-03-18 03:22:34 -0400

    Sending moves

---
+++ README.md
@@ -0,0 +1,16 @@
+# Bomberman Julialang
+
+A bomberman TCP client for this ```https://github.com/aybabtme/bombertcp```
+
+## Done
+
+TCP connection
+Sending moves: up, down, left, right, bomb
+
+## TODO
+
+Connect to bomberman game
+Setup initializer
+Setup JSON parse for response
+Board
+Buffered moves (maybe)
--- bomberman.jl
+++ bomberman.jl
@@ -1,3 +1,45 @@
+using JSON
+
+function up(conn)
+  try
+    write(conn, "up\n")
+  catch err
+    print $err
+  end
+end
+
+function down(conn)
+  try
+    write(conn, "down\n")
+  catch err
+    print $err
+  end
+end
+
+function left(conn)
+  try
+    write(conn, "left\n")
+  catch err
+    print $err
+  end
+end
+
+function right(conn)
+  try
+    write(conn, "right\n")
+  catch err
+    print $err
+  end
+end
+
+function bomb(conn)
+  try
+    write(conn, "bomb\n")
+  catch err
+    print $err
+  end
+end
+
 server = listen(8080)
 while true
   conn = accept(server)
@@ -5,7 +47,11 @@ while true
     try
       while true
         line = readline(conn)
-        write(conn,line)
+        up(conn)
+        down(conn)
+        left(conn)
+        right(conn)
+        bomb(conn)
       end
     catch err
       print $err
